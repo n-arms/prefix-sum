e626e5799b2f1a0e62a040d0b1bd8b33f34685c5
n-arms <75282394+n-arms@users.noreply.github.com>
2024-05-15 20:36:44 -0400

    Implement Sinle-pass decoupled look-back algorithm

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,15 @@ use wgpu::{
     InstanceDescriptor, Limits, PipelineCompilationOptions, RequestAdapterOptions,
 };
 
-use std::{mem::size_of, num::NonZeroU64, time::Instant};
+use core::fmt;
+use std::{iter, mem::size_of, num::NonZeroU64, os::unix::net::UnixDatagram, time::Instant};
 
 fn main() {
     cpu::run();
     pollster::block_on(run());
 }
 
-const SIZE: usize = 256 * 256;
+const SIZE: usize = 1024;
 
 async fn run() {
     // Set up surface
@@ -24,10 +25,10 @@ async fn run() {
     let state = State::new(&adapter).await;
     let buffer = state.ones_buffer(SIZE);
     let start = Instant::now();
-    state.scan(&buffer, SIZE as u32).await;
+    let (workgroup_state, next_block) = state.scan(&buffer, &state.device, SIZE as u32).await;
     println!("gpu compute took {:?}", start.elapsed());
 
-    let data = load_buffer(
+    let prefix_scan = load_buffer(
         &buffer,
         &state.debug_buffer,
         SIZE,
@@ -35,7 +36,35 @@ async fn run() {
         &state.queue,
     )
     .await;
-    println!("\t={}", data.last().unwrap());
+    //print_data(data);
+    //println!("\t={}", data.last().unwrap());
+
+    let states = load_workgroup_state_buffer(
+        &workgroup_state,
+        &state.debug_buffer,
+        SIZE / 4,
+        &state.device,
+        &state.queue,
+    )
+    .await;
+    //print_states(data);
+
+    for (scans, state) in prefix_scan.chunks_exact(4).zip(states) {
+        for scan in scans {
+            print!("{} ", scan);
+        }
+        println!("{:?}", state);
+    }
+
+    let data = load_u32_buffer(
+        &next_block,
+        &state.debug_buffer,
+        1,
+        &state.device,
+        &state.queue,
+    )
+    .await;
+    println!("next block: {}", data.first().unwrap());
 }
 
 struct State {
@@ -43,8 +72,18 @@ struct State {
     queue: wgpu::Queue,
     bind_group_layout: wgpu::BindGroupLayout,
     debug_buffer: wgpu::Buffer,
-    scan_compute_pipeline: wgpu::ComputePipeline,
-    add_compute_pipeline: wgpu::ComputePipeline,
+    pipeline: wgpu::ComputePipeline,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct WorkGroupState {
+    // 0: no work has been done, 1: the aggregate field contains the aggregate, 2: the aggregate is 0, 3: the inclusive prefix field contains the inclusive prefix, 4: the inclusive prefix is 0
+    state: u32,
+    aggregate: f32,
+    inclusive_prefix: f32,
+    debug_info: [u8; 1024],
+    next_byte: u32,
 }
 
 impl State {
@@ -76,13 +115,29 @@ impl State {
                     count: None,
                 },
                 wgpu::BindGroupLayoutEntry {
-                    // sums buffer
+                    // workgroup progress buffer
                     binding: 1,
                     visibility: wgpu::ShaderStages::COMPUTE,
                     ty: wgpu::BindingType::Buffer {
                         ty: wgpu::BufferBindingType::Storage { read_only: false },
                         has_dynamic_offset: false,
-                        min_binding_size: Some(NonZeroU64::new(4).unwrap()),
+                        min_binding_size: Some(
+                            NonZeroU64::new(size_of::<WorkGroupState>() as wgpu::BufferAddress)
+                                .unwrap(),
+                        ),
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    // workgroup progress buffer
+                    binding: 2,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage { read_only: false },
+                        has_dynamic_offset: false,
+                        min_binding_size: Some(
+                            NonZeroU64::new(size_of::<u32>() as wgpu::BufferAddress).unwrap(),
+                        ),
                     },
                     count: None,
                 },
@@ -95,26 +150,17 @@ impl State {
             bind_group_layouts: &[&bind_group_layout],
             push_constant_ranges: &[],
         });
-        let add_compute_pipeline =
-            device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
-                label: Some("Compute Pipeline"),
-                layout: Some(&pipeline_layout),
-                module: &shader,
-                entry_point: "scan_main",
-                compilation_options: PipelineCompilationOptions::default(),
-            });
-        let scan_compute_pipeline =
-            device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
-                label: Some("Compute Pipeline"),
-                layout: Some(&pipeline_layout),
-                module: &shader,
-                entry_point: "add_main",
-                compilation_options: PipelineCompilationOptions::default(),
-            });
+        let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: Some("Compute Pipeline"),
+            layout: Some(&pipeline_layout),
+            module: &shader,
+            entry_point: "main",
+            compilation_options: PipelineCompilationOptions::default(),
+        });
 
         let debug_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Debug Buffer"),
-            contents: bytemuck::cast_slice(&[0f32; SIZE]),
+            contents: bytemuck::cast_slice(&[0f32; 1024 * 1024]),
             usage: BufferUsages::MAP_READ | BufferUsages::COPY_DST,
         });
 
@@ -123,33 +169,43 @@ impl State {
             queue,
             bind_group_layout,
             debug_buffer,
-            add_compute_pipeline,
-            scan_compute_pipeline,
+            pipeline,
         }
     }
 
-    async fn scan(&self, to_scan: &wgpu::Buffer, size: u32) {
+    async fn scan(
+        &self,
+        to_scan: &wgpu::Buffer,
+        device: &wgpu::Device,
+        size: u32,
+    ) -> (wgpu::Buffer, wgpu::Buffer) {
         let mut commands = Vec::new();
-        self.scan_into(to_scan, size, &mut commands).await;
+        let result = self.scan_into(to_scan, device, size, &mut commands).await;
         self.queue.submit(commands);
+        return result;
     }
 
-    #[async_recursion::async_recursion]
     async fn scan_into(
         &self,
         to_scan: &wgpu::Buffer,
+        device: &wgpu::Device,
         size: u32,
         to_execute: &mut Vec<wgpu::CommandBuffer>,
-    ) {
-        let elements_per_workgroup = 64 * 2;
+    ) -> (wgpu::Buffer, wgpu::Buffer) {
+        let elements_per_workgroup = 2 * 2;
         let workgroups = ((size as f32 / elements_per_workgroup as f32).ceil() as u32).max(1);
-        let sums = self
-            .device
-            .create_buffer_init(&wgpu::util::BufferInitDescriptor {
-                label: Some("Sums Buffer"),
-                contents: &vec![0u8; workgroups as usize * size_of::<f32>()],
-                usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
-            });
+        let workgroup_state_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Workgroup State Buffer"),
+            size: (workgroups as usize * size_of::<WorkGroupState>()) as wgpu::BufferAddress,
+            usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
+            mapped_at_creation: false,
+        });
+        let next_block_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Next Block Buffer"),
+            size: size_of::<u32>() as wgpu::BufferAddress,
+            usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
+            mapped_at_creation: false,
+        });
         let bind_group = self.device.create_bind_group(&wgpu::BindGroupDescriptor {
             label: Some("Scan Bind Group"),
             layout: &self.bind_group_layout,
@@ -160,7 +216,11 @@ impl State {
                 },
                 wgpu::BindGroupEntry {
                     binding: 1,
-                    resource: sums.as_entire_binding(),
+                    resource: workgroup_state_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: next_block_buffer.as_entire_binding(),
                 },
             ],
         });
@@ -173,30 +233,13 @@ impl State {
                 timestamp_writes: None,
             });
             pass.set_bind_group(0, &bind_group, &[]);
-            pass.set_pipeline(&self.add_compute_pipeline);
+            pass.set_pipeline(&self.pipeline);
             pass.dispatch_workgroups(workgroups, 1, 1);
         }
 
         to_execute.push(encoder.finish());
 
-        if workgroups > 1 {
-            self.scan_into(&sums, workgroups, to_execute).await;
-
-            let mut encoder = self
-                .device
-                .create_command_encoder(&CommandEncoderDescriptor { label: None });
-            {
-                let mut pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-                    label: None,
-                    timestamp_writes: None,
-                });
-                pass.set_bind_group(0, &bind_group, &[]);
-                pass.set_pipeline(&self.scan_compute_pipeline);
-                pass.dispatch_workgroups(workgroups, 1, 1);
-            }
-
-            to_execute.push(encoder.finish());
-        }
+        return (workgroup_state_buffer, next_block_buffer);
     }
 
     fn ones_buffer(&self, size: usize) -> wgpu::Buffer {
@@ -213,7 +256,43 @@ async fn clear_buffer(buffer: &wgpu::Buffer, length: usize, queue: &wgpu::Queue)
     queue.write_buffer(buffer, 0, &vec![0u8; length * 4]);
     queue.submit([]);
 }
+async fn load_u32_buffer(
+    source: &wgpu::Buffer,
+    temp: &wgpu::Buffer,
+    elements: usize,
+    device: &wgpu::Device,
+    queue: &wgpu::Queue,
+) -> Vec<u32> {
+    let mut encoder = device.create_command_encoder(&CommandEncoderDescriptor { label: None });
 
+    encoder.copy_buffer_to_buffer(
+        source,
+        0,
+        temp,
+        0,
+        (elements * size_of::<u32>()) as wgpu::BufferAddress,
+    );
+    queue.submit(Some(encoder.finish()));
+    let buffer_slice = temp.slice(..);
+
+    let (tx, rx) = futures_intrusive::channel::shared::oneshot_channel();
+    buffer_slice.map_async(wgpu::MapMode::Read, move |result| {
+        tx.send(result).unwrap();
+    });
+    device.poll(wgpu::Maintain::Wait);
+    rx.receive().await.unwrap().unwrap();
+    let data = buffer_slice
+        .get_mapped_range()
+        .as_ref()
+        .chunks_exact(4)
+        .map(|b| u32::from_ne_bytes(b.try_into().unwrap()))
+        .take(elements)
+        .collect::<Vec<_>>();
+
+    temp.unmap();
+
+    data
+}
 async fn load_buffer(
     source: &wgpu::Buffer,
     temp: &wgpu::Buffer,
@@ -244,6 +323,7 @@ async fn load_buffer(
         .as_ref()
         .chunks_exact(4)
         .map(|b| f32::from_ne_bytes(b.try_into().unwrap()))
+        .take(elements)
         .collect::<Vec<_>>();
 
     temp.unmap();
@@ -256,4 +336,99 @@ fn print_data(data: Vec<f32>) {
         print!("{} ", elem);
     }
     println!();
+}
+
+async fn load_workgroup_state_buffer(
+    source: &wgpu::Buffer,
+    temp: &wgpu::Buffer,
+    elements: usize,
+    device: &wgpu::Device,
+    queue: &wgpu::Queue,
+) -> Vec<WorkGroupState> {
+    let mut encoder = device.create_command_encoder(&CommandEncoderDescriptor { label: None });
+
+    encoder.copy_buffer_to_buffer(
+        source,
+        0,
+        temp,
+        0,
+        (elements * size_of::<WorkGroupState>()) as wgpu::BufferAddress,
+    );
+    queue.submit(Some(encoder.finish()));
+    let buffer_slice = temp.slice(..);
+
+    let (tx, rx) = futures_intrusive::channel::shared::oneshot_channel();
+    buffer_slice.map_async(wgpu::MapMode::Read, move |result| {
+        tx.send(result).unwrap();
+    });
+    device.poll(wgpu::Maintain::Wait);
+    rx.receive().await.unwrap().unwrap();
+    let data = buffer_slice
+        .get_mapped_range()
+        .as_ref()
+        .chunks_exact(size_of::<WorkGroupState>())
+        .map(|bytes| {
+            let state = u32::from_ne_bytes(bytes[0..4].try_into().unwrap());
+            let aggregate = f32::from_ne_bytes(bytes[4..8].try_into().unwrap());
+            let inclusive_prefix = f32::from_ne_bytes(bytes[8..12].try_into().unwrap());
+            let debug_info = bytes[12..]
+                .chunks_exact(4)
+                .flat_map(|bytes| u32::from_ne_bytes(bytes.try_into().unwrap()).to_le_bytes())
+                .chain(iter::repeat(0))
+                .take(1024)
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap();
+            WorkGroupState {
+                state,
+                aggregate,
+                inclusive_prefix,
+                debug_info,
+                next_byte: 0,
+            }
+        })
+        .take(elements)
+        .collect();
+
+    temp.unmap();
+
+    data
+}
+
+fn print_states(data: Vec<WorkGroupState>) {
+    for elem in data {
+        print!("{:?} ", elem);
+    }
+    println!();
+}
+
+impl WorkGroupState {
+    pub fn state_name(&self) -> &'static str {
+        match self.state {
+            0 => "Uninit",
+            1 => "Aggregated",
+            2 => "Zero Aggregated",
+            3 => "Prefixed",
+            4 => "Zero Prefixed",
+            _ => "Unknown",
+        }
+    }
+}
+
+impl fmt::Debug for WorkGroupState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let debug: Vec<_> = self
+            .debug_info
+            .iter()
+            .copied()
+            .take_while(|byte| *byte != b'\0')
+            .collect();
+        let debug_str = String::from_utf8(debug).unwrap();
+        f.debug_struct("WorkGroupState")
+            .field("state", &self.state_name())
+            .field("aggregate", &self.aggregate)
+            .field("inclusive_prefix", &self.inclusive_prefix)
+            .field("debug info", &debug_str)
+            .finish()
+    }
 }
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,73 +1,253 @@
+struct WorkGroupState {
+    // 0: no work has been done, 1: the aggregate field contains the aggregate, 2: the aggregate is 0, 3: the inclusive prefix field contains the inclusive prefix, 4: the inclusive prefix is 0
+    state: atomic<u32>,
+    aggregate: atomic<u32>,
+    inclusive_prefix: atomic<u32>,
+    debug_info: array<u32, 256>,
+    next_byte: u32
+}
+
 @group(0)
 @binding(0)
 var<storage, read_write> data: array<f32>;
 
 @group(0)
 @binding(1)
-var<storage, read_write> sums: array<f32>;
+var<storage, read_write> workgroup_state: array<WorkGroupState>;
+
+@group(0)
+@binding(2)
+var<storage, read_write> next_block: atomic<u32>;
 
 
-const wgsize: u32 = 64;
+const wgsize: u32 = 2;
 const n: u32 = wgsize * 2;
 
 var<workgroup> shared_data: array<f32, n>;
 
-@compute
-@workgroup_size(64)
-fn scan_main(
-    @builtin(global_invocation_id) global_id: vec3<u32>, 
-    @builtin(local_invocation_id) local_id: vec3<u32>,
-    @builtin(workgroup_id) workgroup_id: vec3<u32>
-) {
-    let start = workgroup_id.x * n;
-    let tid = local_id.x;
-    var offset: u32 = 1;
-    shared_data[2*tid] = data[start + 2*tid];
-    shared_data[2*tid + 1] = data[start + 2*tid + 1];
+// do a reduction over the given block, stopping each loop iteration to check if the work has already been done
+fn reduce(block: u32, tid: u32) {
+    let start = n * block;
+    let a = data[start + tid];
+    let b = data[start + tid + wgsize];
+    shared_data[tid] = a + b;
 
-    for (var d: u32 = wgsize; d > 0; d = d >> 1) {
-        workgroupBarrier();
+    for (var d: u32 = wgsize >> 1; d > 0; d = d >> 1) {
         if (tid < d) {
-            let a = offset*(2*tid + 1) - 1;
-            let b = offset*(2*tid + 2) - 1;
-            shared_data[b] += shared_data[a];
+            let a = shared_data[tid];
+            let b = shared_data[tid + d];
+            shared_data[tid] = a + b;
         }
-        offset = offset * 2;
+
+        if (tid == 0 && block_finished(block)) {
+            return;
+        }
     }
 
     if (tid == 0) {
-        sums[workgroup_id.x] = shared_data[n - 1];
-        shared_data[n - 1] = 0.0;
+        let value = shared_data[0];
+        let state = workgroup_state[block].state;    
+        if (state != 0) { return; }
+        if (value == 0) {
+            workgroup_state[block].state = 2u;
+        } else {
+            workgroup_state[block].state = 1u;
+            workgroup_state[block].aggregate = bitcast<u32>(value);
+        }
+    }
+}
+
+fn block_finished(block: u32) -> bool {
+    let state = workgroup_state[block].state;
+    let aggregate = bitcast<f32>(workgroup_state[block].aggregate);
+    let inclusive_prefix = bitcast<f32>(workgroup_state[block].inclusive_prefix);
+
+    if (state != 0) {
+        switch (state) {
+            case 1u: {
+                if (aggregate != 0) {
+                    return true;
+                }
+            }
+            case 2u: {return true;}
+            case 3u: {
+                if (inclusive_prefix != 0) {
+                    return true;
+                }
+            }
+            case 4u: {return true;}
+            default: {
+                return false;
+            }
+        }
+    }
+    return false;
+}
+
+fn lookback(block: u32, tid: u32) {
+    if (tid == 0) {
+        prefix = 0.0;
     }
 
-    for (var d: u32 = 1; d < n; d = d * 2) {
-        offset = offset / 2;
+    if (block == 0) {
+        workgroup_state[block].state = 4u;
+        return;
+    }
+
+    var inspecting: u32 = block - 1;
+
+    loop {
+
+        var done: bool = false;
+        var attempt: u32;
+        for (attempt = 0u; attempt < 1024 && !done; attempt = attempt + 1) {
+            let state = workgroup_state[inspecting].state;
+            switch (state) {
+                case 0u: {
+                    reduce(inspecting, tid);
+                }
+                case 1u: {
+                    let aggregate = bitcast<f32>(workgroup_state[inspecting].aggregate);
+                    done = aggregate != 0.0;
+                    if (tid == 0) {
+                        prefix += aggregate;
+                    }
+                }
+                case 2u: {
+                }
+                case 3u: {
+                    let inclusive_prefix = bitcast<f32>(workgroup_state[inspecting].inclusive_prefix);
+                    let aggregate = bitcast<f32>(workgroup_state[inspecting].aggregate);
+                    if (tid == 0) {
+                        prefix += inclusive_prefix + aggregate;
+                    }
+                    return;
+                }
+                case 4u: {
+                    let aggregate = bitcast<f32>(workgroup_state[inspecting].aggregate);
+                    if (tid == 0) {
+                        prefix += aggregate;
+                    }
+                    return;
+                }
+                default: {
+                    done = true;
+                }
+            }
+        }
+        if (inspecting == 0) {
+            return;
+        }
+        inspecting = inspecting - 1;
+        workgroupBarrier();
+    }
+}
+
+fn scan(block: u32, tid: u32, prefix: f32) {
+    let start = block * n;
+    var offset: u32 = 1;
+
+    // copy the data over
+    shared_data[2*tid] = data[start + 2*tid];
+    shared_data[2*tid + 1] = data[start + 2*tid + 1];
+
+    for (var d: u32 = n >> 1; d > 0; d = d >> 1) {
         workgroupBarrier();
         if (tid < d) {
-            let a = offset*(2*tid + 1) - 1;
-            let b = offset*(2*tid + 2) - 1;
-            let temp = shared_data[a];
-            shared_data[a] = shared_data[b];
-            shared_data[b] += temp;
+            let ai = offset*(2*tid + 1) - 1;
+            let bi = offset*(2*tid + 2) - 1;
+            shared_data[bi] = shared_data[bi] + shared_data[ai];
+        }
+        offset = offset * 2;
+    }
+    if (tid == 0) {
+        shared_data[n - 1] = 0.0;
+    }
+    for (var d: u32 = 1; d < n; d = d * 2) {
+        offset = offset >> 1;
+        workgroupBarrier();
+        if (tid < d) {
+            let ai = offset*(2*tid + 1) - 1;
+            let bi = offset*(2*tid + 2) - 1;
+            let temp = shared_data[ai];
+            shared_data[ai] = shared_data[bi];
+            shared_data[bi] = shared_data[bi] + temp;
         }
     }
     workgroupBarrier();
-
-    data[start + 2*tid] = shared_data[2*tid];
-    data[start + 2*tid + 1] = shared_data[2*tid + 1];
+    data[2*tid + start] = shared_data[2*tid] + prefix;
+    data[2*tid + start + 1] = shared_data[2*tid + 1] + prefix;
 }
 
-@compute
-@workgroup_size(64)
-fn add_main(
-    @builtin(global_invocation_id) global_id: vec3<u32>, 
-    @builtin(local_invocation_id) local_id: vec3<u32>,
-    @builtin(workgroup_id) workgroup_id: vec3<u32>
-) {
-    let to_add = sums[workgroup_id.x];
-    let a = workgroup_id.x * n + local_id.x * 2;
-    let b = workgroup_id.x * n + local_id.x * 2 + 1;
+fn get_block() -> u32 {
+    return atomicAdd(&next_block, 1u);
+}
 
-    data[a] = data[a] + to_add;
-    data[b] = data[b] + to_add;
+var<workgroup> block: u32;
+var<workgroup> prefix: f32;
+
+const _a: u32 = 97u;
+const _b: u32 = 98u;
+const _c: u32 = 99u;
+const _d: u32 = 100u;
+const _e: u32 = 101u;
+const _f: u32 = 102u;
+const _g: u32 = 103u;
+const _h: u32 = 104u;
+const _i: u32 = 105u;
+const _j: u32 = 106u;
+const _k: u32 = 107u;
+const _l: u32 = 108u;
+const _m: u32 = 109u;
+const _n: u32 = 110u;
+const _o: u32 = 111u;
+const _p: u32 = 112u;
+const _q: u32 = 113u;
+const _r: u32 = 114u;
+const _s: u32 = 115u;
+const _t: u32 = 116u;
+const _u: u32 = 117u;
+const _v: u32 = 118u;
+const _w: u32 = 119u;
+const _x: u32 = 120u;
+const _y: u32 = 121u;
+const _z: u32 = 122u;
+
+@compute
+@workgroup_size(2)
+fn main(
+    @builtin(local_invocation_id) local_id: vec3<u32>
+) {
+    if (local_id.x == 0) {
+        block = get_block();
+    }
+    workgroupBarrier();
+
+    reduce(block, local_id.x);
+
+    lookback(block, local_id.x);
+
+    if (local_id.x == 0) {
+        if (prefix == 0.0) {
+            workgroup_state[block].state = 4u;
+        } else {
+            workgroup_state[block].state = 3u;
+            workgroup_state[block].inclusive_prefix = bitcast<u32>(prefix);
+        }
+    }
+
+    scan(block, local_id.x, prefix);
+
+    workgroupBarrier();
+}
+
+// only the 8 lowest bits are taken
+fn debug_byte(block: u32, byte: u32) {
+    let next_byte = workgroup_state[block].next_byte;
+    let debug_byte_index = next_byte / 4;
+    let debug_bit_index = next_byte % 4 * 8;
+
+    let bitmask = (byte & 255u) << debug_bit_index;
+    workgroup_state[block].debug_info[debug_byte_index] = workgroup_state[block].debug_info[debug_byte_index] | bitmask;
+    workgroup_state[block].next_byte = workgroup_state[block].next_byte + 1;
 }
